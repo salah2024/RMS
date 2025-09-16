fff026d210dcec19f2d889976a9754c77f1b142b
salah <salah_ahmadi2008@yahoo.com>
2025-09-16 17:42:28 +0330

    change1

---
+++ RMS/Controllers/KhakRizi/KhakRiziController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMS.Controllers.KhakRizi;
+
+public class KhakRiziController(ApplicationDbContext context) : Controller
+{
+    private readonly ApplicationDbContext _context = context;
+
+
+}
